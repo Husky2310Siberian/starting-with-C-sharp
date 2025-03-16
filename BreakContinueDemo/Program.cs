91d90bdbe4224de834785c6f13bf20ea2cfca74a
Siberian <vasileios.kritikos@aueb.gr>
2025-03-16 16:18:47 +0200

    Break-Continue Demo App

---
+++ BreakContinueDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BreakContinueDemo;
+/// <summary>
+/// Example readind numbers 1 -> 10 , with break / continue
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        int j = 0;
+        int sum = 0;
+        
+        Console.WriteLine("Break Example , reads until 7");
+
+        while (true)
+        {
+            Console.WriteLine($"i = {i}");
+            if (i == 7)
+                break;
+            i = i + 1;
+        }
+        
+        Console.WriteLine($"-----------------------------");
+        Console.WriteLine("Continue Example , reads until 10 , WITHOUT reading 5");
+
+        while (j <= 9)
+        {
+            j++;
+            if (j == 5) continue;
+            Console.WriteLine($"j = {j}");
+        }
+        
+    }
+}
